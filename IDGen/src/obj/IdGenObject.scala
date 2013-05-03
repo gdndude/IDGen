32c65af5332d5c0774048108e5f438b86a4bdcb9
gdndude <gdndude@gmail.com>
2013-05-03 13:25:20 -0500

    test

--- IDGen/src/obj/IdGenObject.scala
+++ IDGen/src/obj/IdGenObject.scala
@@ -1,5 +1,14 @@
 package obj
 
-class IdGenObject {
 
+
+abstract class IdGenObject() {
+
+  var guid : Int = 0
+  var name : String = null
+  var superior : IdGenObject = null
+  var subordinate : IdGenObject = null
+  var delegate : IdGenObject = null
+  var credential : Credential = null
+  
 }
